f37011af90304e3c735ed20821b49ba7e51a7b1f
mopsiq <neenset25@gmail.com>
2024-05-26 19:56:26 +0300

    feature: add telegraf

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,51 @@
-import fastify from "fastify";
-import { Telegraf } from "telegraf";
+// import fastify from "fastify";
+// import { Telegraf } from "telegraf";
 
-const bot = new Telegraf(process.env.BOT_TOKEN!);
-const port = (process.env.PORT && +process.env.PORT) || 4040;
-// const webhook = await bot.createWebhook({ domain: webhookDomain });
+// const bot = new Telegraf(process.env.BOT_TOKEN!);
+// const port = (process.env.PORT && +process.env.PORT) || 4040;
+// // let webhook;
+// // const webhook = await bot.createWebhook({
+// //   domain: "https://steam-market-bot.onrender.com",
+// // });
+
+// const server = fastify({
+//   logger: true,
+// });
+
+// server.get("/", async (request, reply) => {
+//   return { message: "Hello, world!" };
+// });
+
+// server.post("/test", async (request, reply) => {
+//   return { message: "Hello, world!" };
+// });
+
+// server.listen({ port, host: "0.0.0.0" }, (err, address) => {
+//   if (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+//   console.log(`Server is listening on ${address}`);
+// });
+
+import { fastify } from "fastify";
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Rest of the code...
+const port = (process.env.PORT && +process.env.PORT) || 6050;
+// console.log("process.env.TELEGRAM_BOT_TOKEN:", process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
+const app = fastify();
+
+bot.on("text", (ctx) => ctx.reply("Hello"));
+
+bot
+  .launch({
+    webhook: { domain: "https://steam-market-bot.onrender.com", port: 2030 },
+  })
+  .then(() => console.log("Webhook bot listening on port", port));
 
 const server = fastify({
   logger: true,
@@ -13,8 +55,8 @@ server.get("/", async (request, reply) => {
   return { message: "Hello, world!" };
 });
 
-server.post("/test", async (request, reply) => {
-  return { message: "Hello, world!" };
+server.post("/new-message", async (request, reply) => {
+  return { message: "Hello, it's a new message!" };
 });
 
 server.listen({ port, host: "0.0.0.0" }, (err, address) => {
