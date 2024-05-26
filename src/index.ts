19747a072b1d2159dc0425979462e54572788c8b
mopsiq <neenset25@gmail.com>
2024-05-26 17:49:11 +0300

    fix: change port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { Telegraf } from "telegraf";
 
 const bot = new Telegraf(process.env.BOT_TOKEN!);
+const port = process.env.PORT || 4040;
 // const webhook = await bot.createWebhook({ domain: webhookDomain });
 
 const server = fastify({
@@ -16,7 +17,7 @@ server.post("/test", async (request, reply) => {
   return { message: "Hello, world!" };
 });
 
-server.listen({ port: 4040 }, (err, address) => {
+server.listen({ port }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
