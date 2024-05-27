528d332ca3063bd4b8478e249a111cadc2252c81
mopsiq <neenset25@gmail.com>
2024-05-28 00:18:00 +0300

    try webhook1

--- src/index.ts
+++ src/index.ts
@@ -1,99 +1,35 @@
-// // import fastify from "fastify";
-// // import { Telegraf } from "telegraf";
-
-// // const bot = new Telegraf(process.env.BOT_TOKEN!);
-// // const port = (process.env.PORT && +process.env.PORT) || 4040;
-// // // let webhook;
-// // // const webhook = await bot.createWebhook({
-// // //   domain: "https://steam-market-bot.onrender.com",
-// // // });
-
-// // const server = fastify({
-// //   logger: true,
-// // });
-
-// // server.get("/", async (request, reply) => {
-// //   return { message: "Hello, world!" };
-// // });
-
-// // server.post("/test", async (request, reply) => {
-// //   return { message: "Hello, world!" };
-// // });
-
-// // server.listen({ port, host: "0.0.0.0" }, (err, address) => {
-// //   if (err) {
-// //     console.error(err);
-// //     process.exit(1);
-// //   }
-// //   console.log(`Server is listening on ${address}`);
-// // });
-
-// import { fastify } from "fastify";
-// import { Telegraf } from "telegraf";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// // Rest of the code...
-// const port = (process.env.PORT && +process.env.PORT) || 6050;
-// // console.log("process.env.TELEGRAM_BOT_TOKEN:", process.env.TELEGRAM_BOT_TOKEN);
-// const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
-// const app = fastify();
-
-// bot.on("text", (ctx) => ctx.reply("Hello"));
-
-// bot
-//   .launch({
-//     webhook: { domain: "https://steam-market-bot.onrender.com", port: 2030 },
-//   })
-//   .then(() => console.log("Webhook bot listening on port", port));
-
-// const server = fastify({
-//   logger: true,
-// });
-
-// server.get("/", async (request, reply) => {
-//   return { message: "Hello, world!" };
-// });
-
-// server.post("/new-message", async (request, reply) => {
-//   return { message: "Hello, it's a new message!" };
-// });
-
-// server.listen({ port, host: "0.0.0.0" }, (err, address) => {
-//   if (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-//   console.log(`Server is listening on ${address}`);
-// });
-
 import { fastify } from "fastify";
+import formBody from "@fastify/formbody";
 import { Bot, webhookCallback } from "grammy";
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = (process.env.PORT && +process.env.PORT) || 6050;
 const token = process.env.TELEGRAM_BOT_TOKEN as string;
-const bot = new Bot(token, {
-  client: { apiRoot: "https://steam-market-bot.onrender.com" },
-});
-
-// const bot = new Bot("", { // <-- use the same token as before
-//   client: { apiRoot: "localhost:8081" },
+const bot = new Bot(token);
+// const bot = new Bot(token, {
+//   client: { apiRoot: "https://steam-market-bot.onrender.com" },
 // });
 
-bot.command("start", (ctx) =>
-  ctx.reply("Welcome! Up and running with webhook.")
-);
-bot.on("message", (ctx) => ctx.reply("It's replaced message!"));
+bot.command("start", (ctx) => {
+  console.log("ctx", ctx);
+  return ctx.reply("Welcome! Up and running.");
+});
+
+bot.on("message", (ctx) => {
+  const chatId = ctx.msg.chat.id;
+  const text = "Hello, world!";
+});
 
 const server = fastify({
   logger: true,
 });
+server.register(formBody);
 
 server.post("/new-message", async (request, reply) => {
   return { message: "Hello, it's a new message!" };
 });
+
 server.post(`/${token}`, webhookCallback(bot, "fastify"));
 
 server.listen({ port, host: "0.0.0.0" }, (err, address) => {
