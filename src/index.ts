a2f301709e095bd010da4a853020271315a79d0f
mopsiq <neenset25@gmail.com>
2024-05-28 09:11:13 +0300

    fix: webhook via fastify

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import { fastify } from "fastify";
 import formBody from "@fastify/formbody";
-import fastifyMiddie from "@fastify/middie";
 import { Bot, webhookCallback } from "grammy";
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = (process.env.PORT && +process.env.PORT) || 6050;
 const token = process.env.TELEGRAM_BOT_TOKEN as string;
 const bot = new Bot(token);
-// const bot = new Bot(token, {
-//   client: { apiRoot: "https://steam-market-bot.onrender.com" },
-// });
 
 bot.command("start", (ctx) => {
   console.log("ctx", ctx);
@@ -21,26 +16,13 @@ bot.command("start", (ctx) => {
 bot.on("message", (ctx) => {
   const chatId = ctx.msg.chat.id;
   const text = "Hello, world!";
+  return ctx.reply(text);
 });
-
-// const app = express();
-// app.use(express.json());
-// app.use(webhookCallback(bot, "express"));
-
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`);
-// });
 
 const server = fastify({
   logger: true,
 });
 server.register(formBody);
-// await server.register(fastifyMiddie);
-// server.use(webhookCallback(bot, "fastify"));
-
-server.post("/new-message", async (request, reply) => {
-  return { message: "Hello, it's a new message!" };
-});
 
 server.post(`/${token}`, webhookCallback(bot, "fastify"));
 
